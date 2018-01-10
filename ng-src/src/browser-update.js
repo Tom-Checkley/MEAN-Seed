79c7fdb111d65731437e42de1129fb43c7c80b1a
Tom Checkley <Tom-Checkley@users.noreply.github.com>
2018-01-10 18:47:20 +0000

    initial file structure

---
+++ ng-src/src/browser-update.js
@@ -0,0 +1,10 @@
+module.exports = () => {
+  var $buoop = { notify: { i: 11, f: -4, o: -4, s: -2, c: -4 }, insecure: true, api: 5 };
+
+  function $buo_f() {
+    var e = document.createElement("script");
+    e.src = "//browser-update.org/update.min.js";
+    document.body.appendChild(e);
+  };
+  try { document.addEventListener("DOMContentLoaded", $buo_f, false) } catch (e) { window.attachEvent("onload", $buo_f) }
+}
