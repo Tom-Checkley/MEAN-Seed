edab2c820c49738a83ae7e8321e85a4cc8083382
Tom Checkley <Tom-Checkley@users.noreply.github.com>
2018-01-11 21:34:48 +0000

    added browser update

--- ng-src/src/app/app.module.ts
+++ ng-src/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -17,4 +17,20 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule implements OnInit {
+
+  ngOnInit() {
+    this.browserUpdate();
+  }
+
+  browserUpdate() {
+    const $buoop = { notify: { i: 11, f: -4, o: -4, s: -2, c: -4 }, insecure: true, api: 5 };
+
+    function $buo_f() {
+      const e = document.createElement('script');
+      e.src = '//browser-update.org/update.min.js';
+      document.body.appendChild(e);
+    }
+    try { document.addEventListener('DOMContentLoaded', $buo_f, false); } catch (e) { (<any>window).attachEvent('onload', $buo_f); }
+  }
+ }
